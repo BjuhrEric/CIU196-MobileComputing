797584e913ffe837cc1948aeaaac5b513615fafa
andreaspegelow <andpegelow@gmail.com>
2017-10-09 15:51:31 +0200

    Added circle class and started first activity

---
+++ Android/MobileComputing/app/src/main/java/com/ciu196/mobilecomputing/ConnectActivity.java
@@ -0,0 +1,34 @@
+package com.ciu196.mobilecomputing;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ConnectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.connect_activity);
+
+        TextView pianoStatusTextView = (TextView) findViewById(R.id.pianoStatusTextView);
+        TextView pianoDetaildTextView = (TextView) findViewById(R.id.pianodetailedTextView);
+
+
+         final Circle actionButton =  (Circle) findViewById(R.id.actionButton);
+
+        actionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //DO STUFF HERE!!!!
+                Toast.makeText(v.getContext(), "ActionClicked",Toast.LENGTH_LONG).show();
+                actionButton.setColor(999999);
+
+
+            }
+        });
+    }
+}
