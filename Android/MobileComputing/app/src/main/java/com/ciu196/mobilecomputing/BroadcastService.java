ed8c67234487c761e7317c978a0e1807007baa4e
andreaspegelow <andpegelow@gmail.com>
2017-10-09 16:00:31 +0200

    Added a dummy backend service

---
+++ Android/MobileComputing/app/src/main/java/com/ciu196/mobilecomputing/BroadcastService.java
@@ -0,0 +1,12 @@
+package com.ciu196.mobilecomputing;
+
+/**
+ * Created by Andreas Pegelow on 2017-10-09.
+ */
+
+public class BroadcastService {
+    public static boolean isLive(){
+        //Todo not implemented yet
+        return true;
+    }
+}
