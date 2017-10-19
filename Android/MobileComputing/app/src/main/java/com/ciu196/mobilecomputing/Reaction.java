d5135241abe597c0c9bacd0b1a3182a102b2e2ff
Alexander Alvmo <alexander.alvmo@outlook.com>
2017-10-19 10:22:04 +0200

    Reaction Serializable?

--- Android/MobileComputing/app/src/main/java/com/ciu196/mobilecomputing/Reaction.java
+++ Android/MobileComputing/app/src/main/java/com/ciu196/mobilecomputing/Reaction.java
@@ -1,9 +1,11 @@
 package com.ciu196.mobilecomputing;
 
+import java.io.Serializable;
+
 /**
  * Created by alexanderalvmo on 2017-10-18.
  */
 
-public enum Reaction {
+public enum Reaction implements Serializable {
     HEART, HAPPY, THUMBS_UP
 }
